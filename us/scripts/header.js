289ce1b495206e58f8c0263bb17b26071ab4529c
Venish Patel <wanicepatel1210>
2020-04-06 00:04:38 -0700

    Overlay navigation bar completed.

---
+++ us/scripts/header.js
@@ -0,0 +1,9 @@
+let openNavBar = function () {
+    let navBar = document.getElementsByClassName("navigation")[0];
+    navBar.style.width = "100%";
+}
+
+let closeNavBar = function () {
+    let navBar = document.getElementsByClassName("navigation")[0];
+    navBar.style.width = "0%";
+}
